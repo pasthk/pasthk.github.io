e4623a34228e0dc3da921b5186e27497289b8fa6
NGKINCHUNG <archaeology.cn@outlook.com>
2023-01-15 20:28:56 +0800

    content updated

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,14 +1,1 @@
 export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-TW\",\"title\":\"追跡香江\",\"description\":\"香港歷史研究索引集\",\"head\":[],\"locales\":{}}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateSiteData) {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ siteData }) => {
-    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
-  })
-}
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
-export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"PastHK\",\"lang\":\"zh-TW\",\"frontmatter\":{\"home\":true,\"title\":\"PastHK\",\"heroText\":\"追跡香江\",\"tagline\":\"香港歷史研究索引集\",\"copyright\":true,\"footer\":false,\"description\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://pasthk.github.io/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"追跡香江\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"PastHK\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-TW\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"PastHK\\\"}\"]]},\"headers\":[],\"readingTime\":{\"minutes\":0.08,\"words\":23},\"filePathRelative\":\"README.md\",\"autoDesc\":true}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
+export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"PastHK\",\"lang\":\"zh-TW\",\"frontmatter\":{\"home\":true,\"title\":\"PastHK\",\"heroText\":\"追跡香江\",\"tagline\":\"香港歷史研究索引集\",\"copyright\":true,\"footer\":false,\"description\":\"\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://pasthk.github.io/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"追跡香江\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"PastHK\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"website\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-TW\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2023-01-15T11:45:27.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2023-01-15T11:45:27.000Z\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"WebPage\\\",\\\"name\\\":\\\"PastHK\\\"}\"]]},\"headers\":[],\"git\":{\"createdTime\":1673529693000,\"updatedTime\":1673783127000,\"contributors\":[{\"name\":\"NGKINCHUNG\",\"email\":\"archaeology.cn@outlook.com\",\"commits\":3}]},\"readingTime\":{\"minutes\":0.08,\"words\":23},\"filePathRelative\":\"README.md\",\"localizedDate\":\"2023年1月12日\",\"autoDesc\":true}")
