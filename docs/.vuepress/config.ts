ec92b5c3b976bed0d5baa76e923d66734102dde7
ngkinchung <archaeology.cn@outlook.com>
2023-01-22 16:25:20 +0800

    minor change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,6 +7,14 @@ export default defineUserConfig({
   title: "追跡香江",
   description: "香港人文與科學研究索引集",
   head:[
+    [
+      "script",
+      {
+        "data-ad-client": "ca-pub-8975507583219124",
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      }
+    ],
     ["link", { rel: "preconnect", href: "https://fonts.googleapis.com" }],
     ["link", { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },],
     ["link", { href: "https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@400&display=swap", rel: "stylesheet", },],
