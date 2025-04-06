168bf4931d07cdb6cc50a93f44b753a77cee1090
citenote <archaeology.cn@outlook.com>
2025-04-06 12:57:32 +0800

    major change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,57 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import sitemap from '@astrojs/sitemap';
+import starlightSidebarTopics from 'starlight-sidebar-topics'
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
-  integrations: [starlight({
-      title: '香江之源',
-	  site: 'https://pasthk.github.io',
-	  base: '/',
-      sidebar: [
-          {
-              label: 'Guides',
-              items: [
-                  { label: 'Example Guide', slug: 'guides/example' },
-              ],
-          },
-      ],
-  }), sitemap()],
+	site: 'https://www.pasthk.com',
+	base: '/',
+	build: {
+		format: 'file',
+	},
+	compressHTML: true,
+	integrations: [
+		starlight({
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			title: '香江之源',
+			description: '考鏡香江源流——香港考古學文獻索引及綜述。',
+			locales: {
+				root: {
+					label: '中文（香港）',
+					lang: 'zh-HK',
+				},
+			},
+			plugins: [
+				starlightSidebarTopics([
+					{
+						label: '序',
+						link: '/foreword.html',
+						items: [
+							'foreword',
+						],
+					},
+					{
+						label: '第一章　香港考古調查',
+						link: '/chapter-one/hong-kong-archaeological-survey-1982-to-1985.html',
+						items: [
+							'chapter-one/hong-kong-archaeological-survey-1982-to-1985', 
+							'chapter-one/hong-kong-archaeological-survey-1995-to-1997', 
+						],
+					},
+					{
+						label: '跋',
+						link: '/afterword.html',
+						items: [
+							'afterword',
+						],
+					},
+				]),
+			],
+		}), 
+		partytown(
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+		),
+	],
 });
