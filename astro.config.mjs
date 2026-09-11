548de1675bb32d3eb596879102908d85b90908df
citenote <pasthk@outlook.com>
2026-09-11 09:47:22 +0800

    change script

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,10 @@ export default defineConfig({
             root: { label: '繁體中文', lang: 'zh-TW' },
         },
         customCss: ['./src/styles/custom.css'],
+        components: {
+            ThemeProvider: './src/components/ThemeProvider.astro',
+            ThemeSelect: './src/components/ThemeSelect.astro',
+        },
         plugins: [
             starlightSidebarTopics([
                 {
