e8daba3c064e7a0deedce3ae6d7223b1e182e917
citenote <pasthk@outlook.com>
2026-09-11 16:39:17 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.pasthk.com/',
+    build: {
+        format: 'file',
+    },
 	integrations: [
 		starlight({
 			title: '香港新聞類編',
+			defaultLocale: 'root',
+			locales: {
+				root: { label: '繁體中文', lang: 'zh-TW' },
+			},
 			sidebar: [
 				{
 					label: 'Reference',
