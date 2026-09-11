d977bf7f214f704e37ae1c4392c85f89f994b99f
citenote <pasthk@outlook.com>
2026-09-11 14:16:06 +0800

    update script

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,6 +21,10 @@ export default defineConfig({
         './src/styles/global.css',
         '@fontsource/noto-serif-tc/600.css',
     ],
+        components: {
+            ThemeProvider: './src/components/LightTheme.astro',
+            ThemeSelect: './src/components/LightThemeSelect.astro',
+        },
       plugins: [
           starlightSidebarTopics([{
                   label: '風俗',
