4f9753c8b2fa80388a95ef449200724d7dbb65fb
citenote <pasthk@outlook.com>
2026-09-11 15:26:35 +0800

    update CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ export default defineConfig({
       },
       customCss: [
         './src/styles/global.css',
+        '@fontsource/noto-serif-tc/600.css',
     ],
         components: {
         ThemeProvider: './src/components/LightThemeProvider.astro',
